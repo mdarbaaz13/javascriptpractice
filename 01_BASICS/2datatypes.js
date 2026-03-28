5a7a549cc5f3c7f32414a747504cd702dd99ce80
Md Arbaaz <mdarbaaz020@gmail.com>
2026-03-28 06:17:50 +0000

    datatypes

---
+++ 01_BASICS/2datatypes.js
@@ -0,0 +1,12 @@
+let name="arbaaz"
+let id=12
+let istrue=true
+let state;
+// null is also a type undefined is also a type 
+console.log(typeof null) // null is treated as object 
+console.log(typeof undefined)//undefined is a special type undefined only 
+console.log(typeof name)
+console.log(typeof id)
+// for materials or reference refer tc39.es standard script or mdn documentation 
+
+
--- 01_BASICS/test.js
+++ 01_BASICS/test.js
@@ -1,1 +1,10 @@
 console.log("arbaaz")
+const accountId = 12345
+let accountName ="MD ARBAAZ"
+var accountType="Savings"
+accountCity="Bengaluru"
+// accountId=234123
+accountName="Arbaaz"
+console.log(accountId)
+console.log(accountName)
+console.table([accountId,accountName,accountType,accountCity])
